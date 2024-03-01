00392cf9d9d2058ad67c91a95daf07830fa5ffe0
KIDSSCC <18147667093@163.com>
2024-03-01 11:26:00 +0000

    add parallel Test

--- tmdb/src/tmdb_test.c
+++ tmdb/src/tmdb_test.c
@@ -11,6 +11,7 @@
 
 #include<thread>
 #include<sys/time.h>
+#include<unistd.h>
 using namespace std;
 //#define CACHELIB_LOCAL
 
@@ -269,6 +270,92 @@ void data_prepare(char*df)
 	fileLoad.close();
 	tdb_close(db);
 }
+void parallel_test(int argc,char*argv[])
+{
+	char* file_path=nullptr;
+	int rand_seed=-1;
+	int o;
+	while((o=getopt(argc,argv,"-f:r:"))!=-1)
+	{
+		switch(o)
+		{
+			case 'f':file_path=optarg;break;
+			case 'r':rand_seed=atoi(optarg);break;
+			default:cout<<"wrong argument\n";break;
+		}
+	}
+	if(!file_path||rand_seed<0)
+	{
+		cout<<"can get efficient argument\n";
+		exit(0);
+	}
+	int total=512*1024;
+	char mode[]="c";
+	TDB* db=tdb_open(file_path,mode);
+	if(!db)
+	{
+		cout<<"Error open: "<<file_path<<endl;
+		exit(0);
+	}
+	//time to set
+	timeval start,end;
+	gettimeofday(&start,NULL);
+
+	int success=0;
+	for(int i=0;i<total;i++)
+	{
+		string key="num_"+to_string(i);
+		string value(1023,char('A'+(i+rand_seed)%26));
+		int s=tdb_store(db,key.c_str(),value.c_str(),TDB_INSERT);
+		if(s==TDB_SUCCESS)
+			success++;
+	}
+
+	gettimeofday(&end,NULL);
+	double start_seconds=timeval_to_seconds(start);
+	double end_seconds=timeval_to_seconds(end);
+	double elapsed_seconds=end_seconds-start_seconds;
+	cout<<"insert success: "<<success<<" failed: "<<total-success<<" used time: "<<elapsed_seconds<<endl;
+	tdb_close(db);
+
+	char mode_r[]="w";
+	TDB* db_r=tdb_open(file_path,mode_r);
+	if(!db_r)
+	{
+		cout<<"Error open: "<<file_path<<endl;
+		exit(0);
+	}
+	//time to get
+	gettimeofday(&start,NULL);
+	success=0;
+	random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<int> dis(0,total-1);
+	for(int i=0;i<5*total;i++)
+	{
+		int num=dis(gen);
+		string key="num_"+to_string(num);
+		//cout<<"in get test, key is: "<<key<<endl;
+		string res_value(1023,char('A'+(num+rand_seed)%26));
+		string get_value=tdb_fetch(db_r,key.c_str());
+		if(res_value==get_value)
+			success++;
+	}
+	gettimeofday(&end,NULL);
+	start_seconds=timeval_to_seconds(start);
+	end_seconds=timeval_to_seconds(end);
+	elapsed_seconds=end_seconds-start_seconds;
+	cout<<"get success: "<<success<<" failed: "<<5*total-success<<" used time: "<<elapsed_seconds<<endl;
+	tdb_close(db_r);
+
+}
+	
+
+
+
+
+
+
 
 void data_run(char*df)
 {
@@ -327,7 +414,7 @@ void data_run(char*df)
 }
 
 
-int main(){
+int main(int argc,char*argv[]){
 
 	
 	//insert&read test
@@ -338,6 +425,7 @@ int main(){
 	
 	
 	printf("============= performance test ===========\n");
+	parallel_test(argc,argv);
 	/*
 	char df[] = "tdb_data_test";
 	int total = 10;
@@ -350,9 +438,9 @@ int main(){
 	kidsscc_db_insert_test(total, df);
 	kidsscc_db_read_test(total, df);*/
 
-	char df[]="ycsb_data";
+	/*char df[]="ycsb_data";
 	data_prepare(df);
-	data_run(df);
+	data_run(df);*/
 	#ifdef CACHELIB_LOCAL
 		facebook::cachelib_examples::destroyCache();
 	#endif

ac51ddf3e916d4e704b89564d777525767f349c7
KIDSSCC <18147667093@163.com>
2024-03-18 11:10:38 +0000

    add some annotation

--- RemoteSchedule/communicate_module.py
+++ RemoteSchedule/communicate_module.py
@@ -6,11 +6,13 @@ import pickle
 receive the current config from bench
 '''
 def receive_config():
+    # create the server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('127.0.0.1', 1412))
     server_socket.listen(1)
-
+    
+    # listening the old_config from the bench
     client_socket, client_address = server_socket.accept()
     received_data = client_socket.recv(1024)
     config = pickle.loads(received_data)
@@ -24,9 +26,11 @@ def receive_config():
 send the new config to bench
 '''
 def send_config(new_config):
+    serialized_config = pickle.dumps(new_config)
+
+    # connect to the bench
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 1413))
-    serialized_config = pickle.dumps(new_config)
     client_socket.send(serialized_config)
     client_socket.close()
     return
@@ -39,7 +43,8 @@ if __name__ == '__main__':
         '''
         eg:
             old_config[0]: ['uniform', 'zipfian']    # name of pool and the turn of pool size and latency
-            old_config[1]: [16, 16]                  # current pool size,64MB per unit,means 16
+            old_config[1]: [16, 16]                  # current pool size,64MB per unit, for example, 16 means current pool size is 1G
+            old_config[2]: [0.0321, 0.0234]          # latency of each workload
         '''
         
         new_config = []
@@ -47,4 +52,9 @@ if __name__ == '__main__':
         new_partition = [old_config[1][0]-2, old_config[1][1]+2]
         new_config.append(new_partition)
 
+        '''
+        eg:
+            new_config[0]: ['uniform', 'zipfian']   # be the same as old_config[0]
+            new_config[1]: [10, 22]                 # the new cache allocation after the resource tuning
+        '''
         send_config(new_config)
